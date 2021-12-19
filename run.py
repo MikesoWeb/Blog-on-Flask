ea0f46e2d5abeba67cca32547a77b91433a2232f
MikesoWeb <klionpsyx@gmail.com>
2021-12-19 14:28:33 +0300

    'delete_info_db'

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         # disable debug mode when deploy on server
-        app.run(debug=True, port=5000, host='0.0.0.0')
+        app.run(debug=False, port=5000, host='0.0.0.0')
 
